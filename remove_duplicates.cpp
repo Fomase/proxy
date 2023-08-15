debe759ad9a53393e8d197cd7dccb3a5cdba03d0
Fomase <82372279+Fomase@users.noreply.github.com>
2023-08-15 17:22:35 +0300

    Update remove_duplicates.cpp

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -4,8 +4,7 @@
 
 void RemoveDuplicates(SearchServer& search_server) {
     for (auto iter = search_server.begin(); iter != search_server.end(); ++iter) {
-        auto& document = *iter;
-        for (auto document_id : search_server.FindDuplicates(document.first)) {
+        for (auto document_id : search_server.FindDuplicates(*iter)) {
             search_server.RemoveDocument(document_id);
         }
     }
