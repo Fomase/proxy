5087e3ef22141b5cc2846b70b30f6d7639995173
Fomase <82372279+Fomase@users.noreply.github.com>
2023-08-15 21:01:49 +0300

    Update remove_duplicates.cpp

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -3,9 +3,12 @@
 #include <set>
 
 void RemoveDuplicates(SearchServer& search_server) {
+    std::set<int> duplicates;
     for (auto iter = search_server.begin(); iter != search_server.end(); ++iter) {
-        for (auto document_id : search_server.FindDuplicates(*iter)) {
-            search_server.RemoveDocument(document_id);
-        }
+        duplicates.merge(search_server.FindDuplicates(*iter));
+    }
+    
+    for (int document_id : duplicates) {
+        search_server.RemoveDocument(document_id);
     }
 }
